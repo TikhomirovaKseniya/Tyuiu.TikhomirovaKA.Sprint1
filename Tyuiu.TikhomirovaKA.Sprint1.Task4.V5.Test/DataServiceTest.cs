0e61ef49cd69e8c38233cf64ec1611f3e7ff9a5a
Ксения Тихомирова <tuxuu.mup@gmail.com>
2025-10-01 02:05:32 +0500

    Task4

---
+++ Tyuiu.TikhomirovaKA.Sprint1.Task4.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.TikhomirovaKA.Sprint1.Task4.V5.Lib;
+
+namespace Tyuiu.TikhomirovaKA.Sprint1.Task4.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 5;
+            double y = 8;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(0.128, res);
+        }
+    }
+}
